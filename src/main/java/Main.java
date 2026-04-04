75a0d079654fff79041d918f20ef02ee84d7e3e9
Dani <danieltraykov@gmail.com>
2026-04-04 17:31:20 +0300

    Style: Apply CSS themes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,6 +28,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        server.createContext("/style.css", exchange -> {
+            InputStream is = Main.class.getClassLoader().getResourceAsStream("css/style.css");
+            if (is == null) {
+                sendResponse(exchange, 404, "text/plain; charset=UTF-8", "CSS not found");
+                return;
+            }
+            String cssContent = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            sendResponse(exchange, 200, "text/css; charset=UTF-8", cssContent);
+        });
+
         server.createContext("/", exchange -> {
             String path = exchange.getRequestURI().getPath();
             if (path.equals("/")) {
