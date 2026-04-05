970c3399a4d10f62e0ef52b7f35c9841fc222241
Dani <danieltraykov@gmail.com>
2026-04-05 14:04:15 +0300

    refactor(handlers): extract root logic to RootHandler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,13 @@ import handler.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-import static util.HttpUtils.*;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         server.createContext("/style.css", new StaticHandler());
-        server.createContext("/", exchange -> {
-            String path = exchange.getRequestURI().getPath();
-            if (path.equals("/")) {
-                exchange.getResponseHeaders().add("Location", "/login");
-                exchange.sendResponseHeaders(302, -1);
-                exchange.close();
-            } else {
-                sendErrorPage(exchange, 404, "The page you are looking for has been moved or does not exist.");
-            }
-        });
-
+        server.createContext("/", new RootHandler());
         server.createContext("/register", new RegisterHandler());
         server.createContext("/login", new LoginHandler());
         server.createContext("/logout", new LogoutHandler());
