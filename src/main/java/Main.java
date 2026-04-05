f6b845cb239c49c78f884143109963328ea842f0
Dani <danieltraykov@gmail.com>
2026-04-05 14:02:28 +0300

    refactor(handlers): extract css logic to StaticHandler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,8 @@
 import com.sun.net.httpserver.HttpServer;
-import core.SessionManager;
 import handler.*;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
 
 import static util.HttpUtils.*;
 
@@ -19,17 +11,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        server.createContext("/style.css", exchange -> {
-            try (InputStream is = Main.class.getClassLoader().getResourceAsStream("css/style.css")) {
-                if (is == null) {
-                    sendResponse(exchange, 404, "text/plain; charset=UTF-8", "CSS not found");
-                    return;
-                }
-                String cssContent = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-                sendResponse(exchange, 200, "text/css; charset=UTF-8", cssContent);
-            }
-        });
-
+        server.createContext("/style.css", new StaticHandler());
         server.createContext("/", exchange -> {
             String path = exchange.getRequestURI().getPath();
             if (path.equals("/")) {
